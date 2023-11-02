9569c224347729f7f686a56fc1eda526ef45d1f2
kiarash.shamaii <kiarash.shamaii@gmail.com>
2023-11-02 18:09:25 +0330

    session2

---
+++ javaReview/session2/exceptionClass/src/main/java/org/example/App.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import sun.reflect.ReflectionFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.LinkedList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+//       try{
+//           FileInputStream f = new FileInputStream(new File("ewewe"));
+//       }catch (Exception e){
+//           e.printStackTrace();
+//       }
+
+        int x = 10 ;
+        int y = 0 ;
+        int z = x/y;
+        System.out.println("this t : " + z);
+        System.out.println( "Hello World!" );
+
+    }
+}
